b2131f5c71a1ef64dcec2e0af778b92ad54b1c2c
nanbantan <104644689+nanbantan@users.noreply.github.com>
2026-04-26 02:04:31 +0000

    Deploying to gh-pages from @ nanbantan/nanbantan.github.io@01facc61a4f461f3f9f2ec9bd36954b87f85cdb3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-talks-amp-presentations",
           title: "talks &amp; presentations",
           description: "",
